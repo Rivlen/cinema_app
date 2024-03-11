ed7219e93ad2c765fbbeb64a2d2f13094f076818
Rivlen <x.stawecki@gmail.com>
2024-03-11 11:41:33 +0100

    Temp fix userbase

--- userbase/urls.py
+++ userbase/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import MemberSignUpView
+# from .views import MemberSignUpView
 
 
 urlpatterns = [
-    path('register/', MemberSignUpView.as_view(), name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    # path('register/', MemberSignUpView.as_view(), name='register'),
+    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
