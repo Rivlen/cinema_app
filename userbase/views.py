1c02bd7e72ee5bcbfbab637085bbd2f980c80df0
Rivlen <x.stawecki@gmail.com>
2024-03-11 12:38:01 +0100

    Update userbase

--- userbase/views.py
+++ userbase/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+
+
+class MemberSignUpView(generic.CreateView):
+    template_name = 'register.html'
